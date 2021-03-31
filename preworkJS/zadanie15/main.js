777b9178c979b1885c42318b7747a9886b32022d
olgasvitlychna <olga.svitlychna9@gmail.com>
2021-03-31 09:48:00 +0200

    dodano sredniki

--- preworkJS/zadanie15/main.js
+++ preworkJS/zadanie15/main.js
@@ -1,6 +1,6 @@
-let paragraph = document.getElementById('text')
-let buttonDodaj = document.getElementById('dodaj')
-let buttonUsun = document.getElementById('usun')
+let paragraph = document.getElementById('text');
+let buttonDodaj = document.getElementById('dodaj');
+let buttonUsun = document.getElementById('usun');
 buttonDodaj.onclick = function(){
     paragraph.textContent = "Lorem, ipsum dolor sit amet consectetur adipisicing elit. Iure aspernatur assumenda in libero voluptatem molestias id atque repudiandae facere ab sint vero voluptas minus minima dolore, praesentium aliquid eligendi quaerat?"
 }
