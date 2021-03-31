63f100b88d81bf60d29449e5ab538755556c7b6b
olgasvitlychna <olga.svitlychna9@gmail.com>
2021-03-31 10:02:52 +0200

    poprawilam kod, dodano warunek dla roznicy oraz iloczynu

--- preworkJS/zadanie16/main.js
+++ preworkJS/zadanie16/main.js
@@ -1,13 +1,25 @@
 function count (param1, param2) {
     let sum = param1 + param2;
     if (sum > 0) {
-        console.log(`Wynik dodawania wynosi ${sum}`)
+        console.log(`Wynik dodawania wynosi ${sum}`);
     }else{
         console.log("Wynik jest nieprawidłowy");
     }
     
-    console.log(`Wynik odejmowania wynosi ${param1-param2}`)
-    console.log(`Wynik mnożenia wynosi ${param1*param2}`)
+    let roznica = param1 - param2;
+    if (roznica > 0) {
+        console.log(`Wynik dodawania wynosi ${roznica}`);
+    }else{
+        console.log("Wynik jest nieprawidłowy");
+    }
+
+    let iloczyn = param1 * param2;
+    if (iloczyn > 0) {
+        console.log(`Wynik dodawania wynosi ${iloczyn}`);
+    }else{
+        console.log("Wynik jest nieprawidłowy");
+    }
+
 }
 
-count(-21,4)
+count(21,-4);
