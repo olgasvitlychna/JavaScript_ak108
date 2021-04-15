8aa35a412b7fa879e3561bf49def87c95393087a
olgasvitlychna <olga.svitlychna9@gmail.com>
2021-04-15 11:46:45 +0200

    warsztat 2 zrobiony +style.css

---
+++ DOM/warsztaty3/js/main.js
@@ -0,0 +1,61 @@
+let newsletterForm = document.getElementById('newsletter-form');
+let allAgree = document.getElementById('all-agree');
+
+
+
+function validate (event){
+
+    let name = document.getElementById('name');
+    let email = document.getElementById('email');
+    let agree1 = document.getElementById('agree1');
+    let errors = document.getElementById('errors');
+    
+    errors.innerHTML = '';
+
+    if(name.value.trim() === ''){
+        let error = document.createElement('li');
+        error.innerText = 'Wpisz Imie i nazwisko!';
+        errors.appendChild(error);
+    }
+
+    if(email.value.trim() === ''){
+        let error = document.createElement('li');
+        error.innerText = 'Wpisz Adres e-mail!';
+        errors.appendChild(error);
+    }
+
+    if(!email.value.includes('@')){
+        let error = document.createElement('li');
+        error.innerText = 'Adres e-mail musi zawierać @';
+        errors.appendChild(error);
+    }
+
+    if (!agree1.checked) {
+        let error = document.createElement('li');
+        error.innerText = 'Nie wyraziłeś Zgody 1!';
+        errors.appendChild(error);
+    }
+
+    if(errors.children.length > 0) {
+        event.preventDefault();
+    }
+
+}
+const allAgreeCheckBox = (event) =>{
+    
+    let agree1 = document.getElementById('agree1');
+    let agree2 = document.getElementById('agree2');
+
+    agree1.checked = event.target.checked;
+    agree2.checked = event.target.checked;
+
+    agree1.disabled = event.target.checked;
+    agree2.disabled = event.target.checked;
+}
+
+
+
+
+
+newsletterForm.addEventListener('submit', validate);
+allAgree.addEventListener('change', allAgreeCheckBox);
